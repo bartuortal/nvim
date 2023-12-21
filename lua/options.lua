6cf1d34c28cf4a21fdfd0fd7c22d7c02bb0d5219
Bartu Ortal <bartuortal@gmail.com>
2023-12-21 20:34:33 +0100

    initial commit

---
+++ lua/options.lua
@@ -0,0 +1,25 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+-- Same as tabstop
+vim.opt.shiftwidth = 0
+
+-- Sync system clipboard with Neovim's clipboard
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true
+
+vim.g.mapleader = " "
